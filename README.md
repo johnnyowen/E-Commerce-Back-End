5648fe86af89e7a98579515b0b441b18056837cb
johnnyowen <j.owen1993@outlook.com>
2023-06-20 09:59:24 -0700

    minimum viable product

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# E-Commerce Back End
+# E-Commerce Back-End
 
-work in progress
+[![License](https://img.shields.io/badge/License-MIT-blueviolet.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+[Description](#description) | [Installation](#installation) | [Usage](#usage) | [License](#license) | [Questions](#questions)
+
+## Description
+
+This is an Express and Sequelize app that is the back end of an e-commerce business. This allows a user to Create, Read, Update and Delete database tables like Products, Tags for prodects and Categories for the products
+
+## Installation
+
+install dependencies and run the app in Insomnia to check it out. The user will also need to create a ".env" file with their MYSQL credentials, or place them directly in the "connections.js" file
+To install dependencies, use this/these command/s: "npm i"
+
+## Usage
+
+Use this as the framework for a business database.
+
+## License
+  
+This repo is licensed under the MIT license. Click on the badge above for more information.
+
+## Questions
+
+If there is any questions feel free to contact me through [GitHub](https://github.com/johnnyowen)
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,96 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// get all categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    }
+  )
+  .then(data => res.json(data))
+  .catch(errr => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+// get one category
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['category_id']
+    }
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
+// create new category
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// update category
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'There is no Category with that ID' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// delete category
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'There is no Category with that ID' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
